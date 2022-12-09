a80ccdab499d359d4a9be3d4f2ef91b588d19f74
tomw <you@example.com>
2022-12-10 00:00:46 +0100

    sandbox

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Safe C - safe(r) version of C language.
 
+Safer version of C89 language - transpiles to C89.
+
 ### About
 
 ### Main features
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,43 @@
-#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+
 #include <boost/program_options.hpp>
 
 namespace po = boost::program_options;
 
+// TODO : logger...
+
 int main(int argc, char **argv)
 {
-	po::options_description desc("Allowed options");
-	desc.add_options() //
-		("help", "print help");
+	po::options_description desc("All SafeC transpiler options:");
+	desc.add_options()																		//
+		("help,h", "print help")															//
+		("file,f", po::value<std::vector<std::string>>(), "SafeC file(s) to be transpiled") //
+		("disable,d", po::value<std::vector<std::string>>(), "disable SafeC options");
 
 	po::variables_map vm;
 	po::store(po::parse_command_line(argc, argv, desc), vm);
 	po::notify(vm);
 
-	if (vm.count("help"))
+	if (vm.count("help") > 0)
 	{
-		printf("help...");
+		std::cout << desc;
+
 		return 0;
 	}
 
-	printf("nothing yet...\n");
+	if (vm.count("disable") > 0)
+	{
+		std::cout << "disabled features: \n";
+		auto &disabledFeatures = vm["disable"].as<std::vector<std::string>>();
+		for (const auto &it : disabledFeatures)
+		{
+			std::cout << " - " << it << std::endl;
+		}
+	}
+
+	std::cout << "not functional yet...\n";
+
 	return 0;
 }
