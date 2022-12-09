02aad3674948815f92be67565aad6ea53670d3b3
tomw <you@example.com>
2022-12-09 23:44:25 +0100

    clang format, cmakes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <cstdio>
+#include <boost/program_options.hpp>
 
-int main()
+namespace po = boost::program_options;
+
+int main(int argc, char **argv)
 {
+	po::options_description desc("Allowed options");
+	desc.add_options() //
+		("help", "print help");
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+
+	if (vm.count("help"))
+	{
+		printf("help...");
+		return 0;
+	}
+
 	printf("nothing yet...\n");
 	return 0;
 }
