9c55933e103af950771c92d0a45b087442ce1404
tomw <you@example.com>
2022-12-16 21:13:30 +0100

    treat warnings as errors

--- src/cmake/CompilerOptions.cmake
+++ src/cmake/CompilerOptions.cmake
@@ -8,6 +8,6 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS 1)
 set(SANITIZERS_FLAGS -fsanitize=address -fsanitize=undefined)
 set(DEBUG_FLAGS -ggdb -O0 ${SANITIZERS_FLAGS})
 
-add_compile_options(-Wall -Wextra -pedantic ${DEBUG_FLAGS})
+add_compile_options(-Wall -Wextra -Werror -pedantic ${DEBUG_FLAGS})
 
 add_link_options(${SANITIZERS_FLAGS})
