bab4b5a2367620f0dc4c5b16fc71d8ee7815cac6
tomw <you@example.com>
2023-09-02 13:53:32 +0200

    sem: delete all old handlers

--- src/parser/semantics/Semantics.hpp
+++ src/parser/semantics/Semantics.hpp
@@ -34,25 +34,6 @@ public:
 
     void handle(const SyntaxChunkType type, const uint32_t stringIndex);
 
-    void handlePostfixExpression(const uint32_t stringIndex, const bool containsArguments);
-    void handleDeferCall(const uint32_t tokenStartStringIndex, const uint32_t stringIndex);
-    void handleReturn(const uint32_t tokenStartStringIndex,
-                      const uint32_t stringIndex,
-                      const bool returnValueAvailable);
-
-    void handleCompoundStatementStart(const uint32_t stringIndex);
-    void handleCompoundStatementEnd(const uint32_t stringIndex);
-    void handleFunctionStart(const uint32_t stringIndex, const bool voidReturnType);
-    void handleFunctionEnd(const uint32_t stringIndex);
-
-    void handleLoopStart(const uint32_t stringIndex);
-    void handleLoopEnd(const uint32_t stringIndex);
-    void handleBreak(const uint32_t tokenStartStringIndex, const uint32_t stringIndex);
-    void handleContinue(const uint32_t tokenStartStringIndex, const uint32_t stringIndex);
-
-    void handleIdentifier(const uint32_t stringIndex, const char *const identifierName);
-    void handleReference(const uint32_t stringIndex);
-
 private:
     SemNodeTranslationUnit mTranslationUnit;
     boost::iostreams::mapped_file_source mSemanticsSourceFile;
