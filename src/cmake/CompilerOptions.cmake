cb8ac350485d0e1f66fbfde16563b7a0ca570809
tomw <you@example.com>
2023-09-30 12:30:58 +0200

    cmake: add option to comment out asan

--- src/cmake/CompilerOptions.cmake
+++ src/cmake/CompilerOptions.cmake
@@ -6,6 +6,8 @@ set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_EXPORT_COMPILE_COMMANDS 1)
 
 set(SANITIZERS_FLAGS -fsanitize=address -fsanitize=undefined)
+# set(SANITIZERS_FLAGS) # use when debugging
+
 set(DEBUG_FLAGS -ggdb -O0 ${SANITIZERS_FLAGS})
 
 add_compile_options(-Wall -Wextra -Werror -pedantic ${DEBUG_FLAGS})
