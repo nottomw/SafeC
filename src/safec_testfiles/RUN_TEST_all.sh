c2631696c2541838eae8fadf7f2524d9c9463539
tomw <you@example.com>
2023-10-01 20:39:28 +0200

    tests: add exit codes

--- src/safec_testfiles/RUN_TEST_all.sh
+++ src/safec_testfiles/RUN_TEST_all.sh
@@ -2,10 +2,37 @@
 
 SCRIPT_NAME=$(basename "$0")
 
+COLOR_RED="\033[31m"
+COLOR_GREEN="\033[32m"
+COLOR_NC="\033[0m"
+
+passed_test_groups=0
+failed_test_groups=0
+
 for i in `ls *.sh`;
 do
     if [ "$i" != "$SCRIPT_NAME" ];
     then
+        echo "Running test group $i..."
         ./$i
+
+        if [ $? -eq 0 ];
+        then
+            ((passed_test_groups++))
+        else
+            ((failed_test_groups++))
+        fi
+
+        echo ""
     fi
 done
+
+echo ""
+if [ "$failed_test_groups" -eq 0 ];
+then
+    echo -e "[+] ${COLOR_GREEN}test groups passed: $passed_test_groups, failed: $failed_test_groups${COLOR_NC}"
+    exit 0
+else
+    echo -e "[-] ${COLOR_RED}test groups passed: $passed_test_groups, failed: $failed_test_groups${COLOR_NC}"
+    exit 1
+fi
--- src/safec_testfiles/RUN_TEST_ast_generation.sh
+++ src/safec_testfiles/RUN_TEST_ast_generation.sh
@@ -54,6 +54,8 @@ echo ""
 if [ "$tests_failed" -eq 0 ];
 then
     echo -e "[+] ${COLOR_GREEN}passed: $tests_passed, failed: $tests_failed${COLOR_NC}"
+    exit 0
 else
     echo -e "[-] ${COLOR_RED}passed: $tests_passed, failed: $tests_failed${COLOR_NC}"
+    exit 1
 fi
