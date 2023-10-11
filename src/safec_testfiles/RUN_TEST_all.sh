379157fa2e6a7fdcf8bfb228f196f51635d1c347
tomw <you@example.com>
2023-10-11 17:46:26 +0200

    tests: add option to regenerate all test cases

--- src/safec_testfiles/RUN_TEST_all.sh
+++ src/safec_testfiles/RUN_TEST_all.sh
@@ -6,8 +6,30 @@ COLOR_RED="\033[31m"
 COLOR_GREEN="\033[32m"
 COLOR_NC="\033[0m"
 
+if [ -z "$SAFEC_PATH" ];
+then
+    SAFEC_PATH=../../build/bin/SafeCTranspiler
+fi
+
 passed_test_groups=0
 failed_test_groups=0
+
+if [ "$1" = "regen" ];
+then
+    echo -e "${COLOR_RED}REGENERATING ALL TEST CASES${COLOR_NC}"
+
+    for i in `ls AST*`;
+    do
+        ${SAFEC_PATH} -f $i -o . -a -n > ./testfiles_generated_asts/${i##*/}.AST
+        ${SAFEC_PATH} -f $i -o . -c -n > ./testfiles_generated_coverage/${i##*/}.COV
+    done
+
+    ${SAFEC_PATH} -f AST_defer.sc -o /tmp -n --astdump-mod --generate > testfiles_generated_defer_ast/AST_defer.sc.AST
+
+    echo -e "${COLOR_GREEN}REGENERATING ALL TEST CASES DONE${COLOR_NC}"
+
+    exit 0
+fi
 
 for i in `ls *.sh`;
 do
