ed26ee8317b4de523f63f68cd7e6a1591d248a6f
tomw <you@example.com>
2023-10-11 18:42:55 +0200

    tests: compare output c file too

--- src/safec_testfiles/RUN_TEST_defer_modified_ast.sh
+++ src/safec_testfiles/RUN_TEST_defer_modified_ast.sh
@@ -6,10 +6,17 @@ SCRIPT_NAME=$(basename "$0")
 TMP_FILE_PREFIX=/tmp/safec_defer_ast_test_file_
 GENERATED_DIR=testfiles_generated_defer_ast
 GENERATED_FILE_EXT=AST
+GENERATED_C_SOURCE_DIR=/tmp/safec_c_source
 
 if [ -z "$SAFEC_PATH" ];
 then
     SAFEC_PATH=../../build/bin/SafeCTranspiler
+fi
+
+if [ ! -d "$GENERATED_C_SOURCE_DIR" ];
+then
+    echo "Creating C source generation dir: $GENERATED_C_SOURCE_DIR..."
+    mkdir $GENERATED_C_SOURCE_DIR
 fi
 
 AST_FILE_PREFIX="AST_defer"
@@ -31,7 +38,7 @@ do
             if [ -e "$FILE_GENERATED" ];
             then
                 echo "[+] Defer modified AST check for $file..."
-                $($SAFEC_PATH -f $file -o /tmp -n --astdump-mod --generate > $TMP_FILE_PREFIX$file)
+                $($SAFEC_PATH -f $file -o $GENERATED_C_SOURCE_DIR -n --astdump-mod --generate > $TMP_FILE_PREFIX$file)
                 diff_output=`diff -q $FILE_GENERATED $TMP_FILE_PREFIX$file`
                 if [ "$diff_output" = "" ];
                 then
@@ -43,7 +50,34 @@ do
                     echo "[-]     - diff $FILE_GENERATED $TMP_FILE_PREFIX$file"
                     echo "[-]     - kdiff3 $FILE_GENERATED $TMP_FILE_PREFIX$file"
                     echo "[-]    run to regenerate:"
-                    echo "[-]     - $SAFEC_PATH -f $file -o /tmp -n --astdump-mod --generate > $FILE_GENERATED"
+                    echo "[-]     - $SAFEC_PATH -f $file -o $GENERATED_C_SOURCE_DIR -n --astdump-mod --generate > $FILE_GENERATED"
+                    ((tests_failed++))
+                fi
+
+                file_no_extension="${file%.*}"
+                FILE_GENERATED_C_SOURCE="$GENERATED_DIR/$file_no_extension.c"
+                FILE_GENERATED_C_SOURCE_PATTERN=$GENERATED_C_SOURCE_DIR/$file_no_extension.c
+                if [ -e "$FILE_GENERATED_C_SOURCE" ] && [ -e $FILE_GENERATED_C_SOURCE_PATTERN ];
+                then
+                    echo "[+] Defer generated .c file check for file $file..."
+                    diff_output=`diff -q $FILE_GENERATED_C_SOURCE $FILE_GENERATED_C_SOURCE_PATTERN`
+                    if [ "$diff_output" = "" ];
+                    then
+                        echo -e "[+] ${COLOR_GREEN}passed${COLOR_NC}"
+                        ((tests_passed++))
+                    else
+                        echo -e "[-] ${COLOR_RED}FAILED${COLOR_NC}"
+                        echo "[-]    run to see differences:"
+                        echo "[-]     - diff $FILE_GENERATED_C_SOURCE $FILE_GENERATED_C_SOURCE_PATTERN"
+                        echo "[-]     - kdiff3 $FILE_GENERATED_C_SOURCE $FILE_GENERATED_C_SOURCE_PATTERN"
+                        echo "[-]    run to regenerate:"
+                        echo "[-]     - $SAFEC_PATH -f $file -o $GENERATED_C_SOURCE_DIR -n --astdump-mod --generate > /dev/null"
+                        ((tests_failed++))
+                    fi
+                else
+                    echo -e "[-] ${COLOR_RED}either the pattern file does not exist or transpiler did not generate .c file${COLOR_NC}"
+                    echo -e "\tGenerated file location: $FILE_GENERATED_C_SOURCE"
+                    echo -e "\tPattern file location: $FILE_GENERATED_C_SOURCE_PATTERN"
                     ((tests_failed++))
                 fi
             fi
---
+++ src/safec_testfiles/testfiles_generated_defer_ast/AST_defer.c
@@ -0,0 +1,215 @@
+#include <stdio.h>
+
+void DEFER_FUNCTION_SCOPE(void)
+{
+    printf("function scope 1\n");
+    printf("function scope 2\n");
+ printf("function scope 3\n");
+}
+
+void DEFER_SIMPLE_SCOPE_ONLY(void)
+{
+    {
+        printf("scope 1\n");
+        printf("scope 2\n");
+ printf("scope 3\n");
+    }
+}
+
+void DEFER_SIMPLE_SCOPE(void)
+{
+    printf("function scope start\n");
+
+    {
+        printf("scope 1\n");
+        printf("scope 2\n");
+ printf("scope 3\n");
+    }
+
+    printf("function scope end\n");
+}
+
+void DEFER_CONDITION(int someParam)
+{
+    printf("fun start\n");
+
+    if (someParam == 5)
+    {
+        printf("1\n");
+        printf("2\n");
+ printf("3\n");
+    }
+
+    printf("fun end\n");
+}
+
+void DEFER_CONDITION_MULTIPLE_NESTED(int someParam)
+{
+
+    if (someParam > 10)
+    {
+        if (someParam < 5)
+        {
+            printf("1\n");
+ printf("2\n");
+        }
+ printf("3\n");
+    }
+ printf("last");
+}
+
+void DEFER_CONDITION_MULTIPLE(int someParam)
+{
+
+    if (someParam > 10)
+    {
+        printf("1.1");
+ printf("1.2\n");
+    }
+
+    if (someParam < 5)
+    {
+        printf("2.1\n");
+ printf("2.2\n");
+    }
+ printf("last");
+}
+
+void DEFER_LOOP(void)
+{
+    int i = 0;
+    for (i = 0; i < 5; i++)
+    {
+        printf("1\n");
+ printf("2\n");
+    }
+
+    i = 0;
+    while (i < 5)
+    {
+        printf("1\n");
+        i++;
+ printf("2\n");
+    }
+}
+
+void DEFER_LOOP_BREAK(int param)
+{
+    int i = 0;
+    for (i = 0; i < 5; i++)
+    {
+
+        if (i == param)
+        {
+            break;
+        }
+
+        printf("1\n");
+ printf("2\n");
+    }
+
+    i = 0;
+    while (i < 5)
+    {
+
+        if (i == param)
+        {
+            break;
+        }
+
+        printf("1\n");
+        i++;
+ printf("2\n");
+    }
+}
+
+void DEFER_LOOP_CONTINUE(int param)
+{
+    int i = 0;
+    for (i = 0; i < 5; i++)
+    {
+
+        if (i == param)
+        {
+            continue;
+        }
+
+        printf("1\n");
+ printf("2\n");
+    }
+
+    i = 0;
+    while (i < 5)
+    {
+
+        if (i == param)
+        {
+            continue;
+        }
+
+        printf("1\n");
+        i++;
+ printf("2\n");
+    }
+}
+
+void DEFER_MULTIPLE(void)
+{
+ printf("1\n");
+ printf("2\n");
+ printf("3\n");;
+}
+
+void DEFER_SWITCH_CASE(int param)
+{
+
+    switch (param)
+    {
+        case 0:
+            printf("0\n");
+            break;
+
+        case 1:
+            printf("0\n");
+            break;
+
+        default:
+            printf("default\n");
+            break;
+    }
+ printf("last\n");
+}
+
+void DEFER_STRANGE(void)
+{
+    int i = 0;
+
+    // no-scope loops and conds
+
+    for (i = 0; i < 5; i++)
+ printf("%d\n", i);
+
+    if (i == 0)
+ printf("deferred print in cond\n");
+ printf("last statement in function\n");;
+}
+
+void DEFER_WITH_COMPL_STATEMENTS(void)
+{
+    int counter = 0;
+    int counterSecond = 5;
+
+    printf("fun start\n");
+
+    {
+        printf("counter: %d\n", counter);
+ counter += 1;;
+    }
+    printf("counter: %d\n", counter);
+
+    {
+        printf("counter: %d\n", counter);
+ counter = counterSecond + counter;;
+    }
+    printf("counter: %d\n", counter);
+}
